072b1fb3f63fa3cfd695d6ca5df9c02366620f46
Prithvirajsinh07 <pgdodiya7208@gmail.com>
2024-06-13 10:38:15 +0530

    first commit

---
+++ indicator.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main(){
+//  char name[50];
+//  int count=0;
+//  printf("Enter Your Name : ");
+//  fgets(name,50,stdin);
+// for(int i=0;name[i]!='\0';i++){
+//     count++;
+// }
+// count-=1;
+//  printf("your name of number is  %d : ",count);
+
+// int row;
+// int cols;
+// printf("enter the number");
+// scanf("%d",&row);
+// scanf("%d",&cols);
+// int mutiplyNumber[row][cols];
+// int number[row][cols];
+// for(int i=0;i<row;i++){
+//     for(int j=0;j<cols;j++){
+//        printf("your arr %d is %d : ",i,j);
+//        scanf("%d",&mutiplyNumber[i][j]);
+//     }
+// }
+//     for(int i=0;i<row;i++){
+//     for(int j=0;j<cols;j++){
+      
+//        printf("%d",mutiplyNumber[i][j]);
+//     }
+//  printf("\n");
+// }
+// for(int i=0;i<row;i++){
+//     for(int j=0;j<cols;j++){
+//         number[i][j]=mutiplyNumber[i][j]*mutiplyNumber[i][j]*mutiplyNumber[i][j];
+//     }
+
+// }
+// for(int i=0;i<row;i++){
+//     for(int j=0;j<cols;j++){
+//        printf("%d ",number[i][j]);
+//     }
+// printf("\n");
+// }
+
+
+
+ return 0;
+
+}
+
+
+
